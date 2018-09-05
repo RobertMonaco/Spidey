dca315c6e1a0aeb1c0b6c28f6cca9e9adf4b98aa
RobertMonaco <robertmonaco@ou.edu>
2018-09-05 12:36:06 -0500

    Added spidey.py placeholder

---
+++ spidey.py
@@ -0,0 +1,10 @@
+#Spidey.py
+#Robert Monaco - Cody Poteet - Daniel Schon - Daniel Chambers - Lilia Williams
+#Software Engineer I - Rafal Jabrzemski
+######################################################################################
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
