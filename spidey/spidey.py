a45b0896ad7c7370f9e7123b40243ebb6469d741
RobertMonaco <robertmonaco@ou.edu>
2018-09-06 15:15:53 -0500

    Uncommented spidey.py logic

--- spidey/spidey.py
+++ spidey/spidey.py
@@ -4,29 +4,29 @@
 ######################################################################################
 import io
 import os
-#import google-cloud-vision
-#from google.cloud.vision import types
-#from google.cloud.vision import ImageAnnotatorClient
-#import PIL
-#from PIL import Image
+import google-cloud-vision
+from google.cloud.vision import types
+from google.cloud.vision import ImageAnnotatorClient
+import PIL
+from PIL import Image
 
 
 def analyze(file_path):
 
     # Pass image into Google API
     # Instatiate client
-#    cl = ImageAnnotatorClient()
+    cl = ImageAnnotatorClient()
 
     # Load image into memory
-#    with io.open(file_path, 'rb') as img_file:
-#        img_content = img_file.read()
+    with io.open(file_path, 'rb') as img_file:
+        img_content = img_file.read()
 
     # Import image
-#    img = types.Image(content=img_content)
+    img = types.Image(content=img_content)
 
     # Get response from client, assign labels
-#    resp = cl.label_detection(image=img)
-    labels = ['a', 'b', 'c'] #resp.label_annotations
+    resp = cl.label_detection(image=img)
+    resp.label_annotations
 
     # return results
     return labels
