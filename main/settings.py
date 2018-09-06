f2f953f0e7352687eefbd50656f7d23f0670284c
dlschon <dlschon0@gmail.com>
2018-09-06 08:59:37 -0500

    fixed mysterious missing statics error

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0mhe&et)x70-4tb+0==w$*+_u-f%&&*hozdguu7zlkkn+g7mqf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Application definition
 
