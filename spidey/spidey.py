e416bea89d2576f2363c00e420a84e84a7a184e8
robertmonaco <robertmonaco@ou.edu>
2018-09-09 12:16:10 -0500

    Changed spidey.py to pull web descriptions

--- spidey/spidey.py
+++ spidey/spidey.py
@@ -4,6 +4,7 @@
 ######################################################################################
 import io
 import os
+import json
 import google-cloud-vision
 from google.cloud.vision import types
 from google.cloud.vision import ImageAnnotatorClient
@@ -11,7 +12,36 @@ import PIL
 from PIL import Image
 
 
+class Spider:
+    def __init__(self):
+        self.com_name = ""
+        self.sci_name = ""
+        self.type = ""
+        self.help = ""
+
+    def __init__(self,name,latin,venomous,bit_text):
+        self.com_name = name
+        self.sci_name = latin
+        self.type_spider = venomous
+        self.help = bit_text
+
+    def com_name(self):
+        return self.com_name
+
+    def sci_name(self):
+        return self.sci_name
+
+    def type_spider(self):
+        return self.type_spider
+
+    def help(self):
+        return self.help
+
+
+
 def analyze(file_path):
+    spider_json_path = 'spiders.JSON'
+    spider_dict = json.loads(open(spider_json_path).read())
 
     # Pass image into Google API
     # Instatiate client
@@ -25,8 +55,19 @@ def analyze(file_path):
     img = types.Image(content=img_content)
 
     # Get response from client, assign labels
-    resp = cl.label_detection(image=img)
-    resp.label_annotations
+    resp = cl.web_detection(image=img)
+    annotations = resp.web_detection
 
+    if annotations.web_entities:
+        is_spider = False
+        for entity in annotations.web_entities:
+            if str(entity.description) in spider_dict:
+                #Accepted spider
+                return Spider(str(entity.description),spider_dict[str(entity.description)]["Scientific Name"]
+                    ,spider_dict[str(entity.description)]["Type"],spider_dict[str(entity.description)]["Help"])
+            if str(entity.description) == "Spider":
+                is_spider = True
+        if is_spider:
+            
     # return results
     return labels
