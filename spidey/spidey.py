133097cc60da7650e2c33eb79126d4aff40e3d8c
robertmonaco <robertmonaco@ou.edu>
2018-09-09 13:31:10 -0500

    Added JSON file and spidey object fix

--- spidey/spidey.py
+++ spidey/spidey.py
@@ -18,16 +18,18 @@ MY_BUCKET = 'cs4263spidey'
 
 class Spider:
     def __init__(self):
-        self.com_name = ""
-        self.sci_name = ""
-        self.type = ""
-        self.help = ""
+        self.com_name = "Unidentified Spider"
+        self.sci_name = "Unknown"
+        self.type = "Unknown"
+        self.help = "If bitten by an unidentified spider you should clean the wound, apply a compress, and consult a medical professional if pain persists."
+        self.icon_path = "static/icons/unknown.png"
 
-    def __init__(self,name,latin,venomous,bit_text):
+    def __init__(self,name,latin,venomous,bit_text, icon_path):
         self.com_name = name
         self.sci_name = latin
         self.type_spider = venomous
         self.help = bit_text
+        self.icon_path = icon_path
 
     def com_name(self):
         return self.com_name
@@ -40,6 +42,9 @@ class Spider:
 
     def help(self):
         return self.help
+
+    def icon_path(self):
+        return self.icon_path
 
 
 
@@ -66,14 +71,19 @@ def analyze(file_path):
 
     if annotations.web_entities:
         is_spider = False
+        
         for entity in annotations.web_entities:
             if str(entity.description) in spider_dict:
                 #Accepted spider
                 return Spider(str(entity.description),spider_dict[str(entity.description)]["Scientific Name"]
-                    ,spider_dict[str(entity.description)]["Type"],spider_dict[str(entity.description)]["Help"])
+                    ,spider_dict[str(entity.description)]["Type"],spider_dict[str(entity.description)]["Help"],"static/icons/" + spider_dict[str(entity.description)]["Type"].lower() +'.png')
             if str(entity.description) == "Spider":
                 is_spider = True
+        
         if is_spider:
+            return Spider()
+        else:
+            return Spider("", "","Uhhh...","Spidey could not identify a spider in this picture","status/icons/notaspider.png")
             
     # return results
     return labels
