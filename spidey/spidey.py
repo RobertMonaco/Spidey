08ba49ee9645e430123a0d18977f6ae92a9319b7
robertmonaco <robertmonaco@ou.edu>
2018-09-10 08:45:03 -0500

    Added spidey.py constructor for Spider fix

--- spidey/spidey.py
+++ spidey/spidey.py
@@ -16,14 +16,7 @@ from django.core.files.storage import default_storage
 MY_BUCKET = 'cs4263spidey'
 
 class Spider:
-    def __init__(self):
-        self.com_name = "Unidentified Spider"
-        self.sci_name = "Unknown"
-        self.type = "Unknown"
-        self.help = "If bitten by an unidentified spider you should clean the wound, apply a compress, and consult a medical professional if pain persists."
-        self.icon_path = "static/icons/unknown.png"
-
-    def __init__(self,name,latin,venomous,bit_text, icon_path):
+    def __init__(self,name = "Unidentified Spider",latin = "Unknown",venomous = "Unknown",bit_text = "If bitten by an unidentified spider you should clean the wound, apply a compress, and consult a medical professional if pain persists.", icon_path = "static/icons/unknown.png"):
         self.com_name = name
         self.sci_name = latin
         self.type_spider = venomous
